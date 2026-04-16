a7cb2f2840848bdf5bdc12dca7313694c7f30df8
IvanDr1 <ivandrpriv@gmail.com>
2026-04-16 14:20:08 +0200

    Fix: index.html apuntando a src en lugar de assets viejos

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Al ser repositorio de usuario (nombre.github.io), se queda así.
+  base: "/", 
 });
